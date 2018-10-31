run ok, but the recall and precision is not correct,

dataset.py
@@ -8,10 +8,10 @@ from sklearn.datasets import make_moons
 
 
 ###load dataset;
-@classmethod
+
 @func_track
 def load_date():
-    x_moons, y_moons=make_moons(m=2000,noise=0.1,random_state=42)
+    x_moons, y_moons=make_moons(n_samples=2000,noise=0.1,random_state=42)
     return x_moons, y_moons
 
 data,label=load_date()
@@ -21,15 +21,15 @@ batch_n= int(np.ceil(len(data) / batch_size))
 ###data separate
 test_ratio = 0.2
 test_size = int(len(data) * test_ratio)
-X_train = data[:-test_size]
-X_test = data[-test_size:]
+x_train = data[:-test_size]
+x_test = data[-test_size:]
 y_train = label[:-test_size]
 y_test = label[-test_size:]
 
 
 ### pick batch
-@classmethod
-@func_track
+
+
 def random_batch(x_,y_, size):
     random_indices=np.random.randint(0,len(x_),size)#random
     x_batch=x_[random_indices]

net_design.py
@@ -14,10 +14,9 @@ class network:
 
     #dense_layer
     @classmethod
-    @func_track
-    def dense_layer(cls,input_tensor,hedden_number,name_):
+    def dense_layer(cls,input_tensor,hidden_number,name_):
         regularization = tf.contrib.layers.l2_regularizer(lambd) #对fc网络使用正则化
-        fc = tf.layers.dense(input_tensor,hedden_number,activation=tf.nn.relu,
+        fc = tf.layers.dense(input_tensor,hidden_number,activation=tf.nn.relu,
                             kernel_regularizer=regularization,name=name_)
         return fc
 

train_test.py
@@ -11,18 +11,14 @@ import os
 
 
 
-logging.info("**********************mason_p nn_design(%s)***********************"%TM)
-
-
 def main():
     with tf.device(device),tf.Session() as sess:
         ###input data / placeholder
-        X=tf.placeholder(tf.float32,shape=(None))
-        Y=tf.placeholder(tf.float32,shape=(None))
-        input=X
+        X=tf.placeholder(tf.float32,shape=(None,x_train.shape[1]))
+        Y=tf.placeholder(tf.int32)
 
         ###set up net /build net
-        net=net_design.network.net_build(input)
+        net=net_design.network.net_build(input_=X, output_dim=classes)
 
         ###loss func
         cross_entropy=tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y,logits=net["output"])
@@ -37,8 +33,8 @@ def main():
         ###acc
         #acc is not for/contain train, it comes from test, test dataset
         prediction=tf.argmax(net["output"],1) # get the index of the max one
-        #Accuracy= tf.nn.in_top_k(net["output"], Y,1)#y is ground_truth,
-        correct_prediction=tf.equal(prediction,tf.argmax(Y,1))
+        correct_prediction= tf.nn.in_top_k(net["output"], Y,1)#y is ground_truth,
+        #correct_prediction=tf.equal(prediction,tf.argmax(Y,1))
         acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32)) #reduce_mean get the average of acc; tf.cast change the data type
         acc_summary=tf.summary.scalar("acc",acc)
 
@@ -46,14 +42,18 @@ def main():
         ###sess.run/ begin to cal
         #checkpoint
         saver = tf.train.Saver(max_to_keep=5) #keep last 4
+        #summary writer
+        train_writer = tf.summary.FileWriter(train_logdir)
+        test_writer = tf.summary.FileWriter(test_logdir)
+
         #initial variable,
-        if os.path.isfile(checkpoint_iter_path):
-            with open(checkpoint_iter_path, "rb") as f:
-                iter_i = int(f.read())# the number of lines is the iter
-                epoch_start = iter_i/batch_n
+        if os.path.isfile(iter_counter_path):
+            with open(iter_counter_path, "rb") as f:
+                iter_i = int(f.read())
+                epoch_start = int(np.ceil(iter_i / batch_n))
                 batch_start= iter_i%batch_n
             logging.info("Training was interrupted. Continuing at iter: {}".format(iter_i))
-            saver.restore(sess, checkpoint_path)
+            saver.restore(sess, checkpoint_save_path)
         else:
             epoch_start=0
             batch_start=0
@@ -62,14 +62,16 @@ def main():
         for epoch_i in range(epoch_start,epoch_n): # start from 0 to (epoch_n -1)
             for batch_i in range(batch_start,batch_n):
                 iter= batch_n* epoch_i + batch_i # 1 iter is one batch pass through cal;iter starts from 1, not 0
-
+                # iter counter
+                with open(iter_counter_path, "wb") as f:
+                    f.write(b"%d" % iter)  # b mean binary
 
                 if iter % show_iter!=0:
                     #train
                     x_batch,y_batch=random_batch(x_train,y_train,batch_size)
                     train_merged=tf.summary.merge_all()
                     train_summary, _ = sess.run([train_merged,train_op],feed_dict={X: x_batch,Y: y_batch})
-                    train_writer = tf.summary.FileWriter(train_logdir)
+
                     try:
                         train_writer.add_summary(train_summary, iter)
                     except:
@@ -80,27 +82,26 @@ def main():
                 else:
                     #test, every 100iter
                     test_merged=tf.summary.merge([loss_summary,acc_summary]) #or test_merged= tf.summary.merge_all()
-                    test_summary,test_loss, test_acc, test_pred, test_crt_pred,t= sess.run([test_merged,loss, acc, predictiton,correct_prediction],
-                                                                              feed_dirt={X: x_test, Y: y_test})
+                    test_summary,test_loss, test_acc, test_pred, test_crt_pred= sess.run([test_merged,loss, acc, prediction,correct_prediction],
+                                                                              feed_dict={X: x_test, Y: y_test})
                     # do not need batch, as the testset is much small
 
-                    test_writer = tf.summary.FileWriter(test_logdir)
                     try:
                         test_writer.add_summary(test_summary, global_step=iter)
                     except:
                         test_writer.closs()
                         logging.error("add test summary failed, closed writer")
-                    test_precision_score=precision_score(y_test,test_pred)
-                    test_recall_score=recall_score(y_test,test_pred)
+
+                    #logging.info("{}\n{}\n{}".format(y_test, test_pred,test_crt_pred))
+                    test_precision_score=precision_score(y_test,test_crt_pred)
+                    test_recall_score=recall_score(y_test,test_crt_pred)
                     logging.info("epoch:{0}\titer:{1}\ttest_loss:{2}\ttest_acc:{3}\tprecision_score:{4}\trecall_score:{5}"
                                  .format(epoch_i,iter,test_loss,test_acc,test_precision_score,test_recall_score))
 
                     #save iter model
                     if iter%checkpoint_iter==0 and iter/checkpoint_iter >=0:
-                        saver.save(sess,checkpoint_path,global_step=iter)#the global_step tell which model to save
-                        # iter counter
-                        with open(checkpoint_iter_path,"wb") as f:
-                            f.write(b"%d" %iter) #b mean binary
+                        saver.save(sess,checkpoint_save_path,global_step=iter)#the global_step tell which model to save
+
 
 
 
@@ -119,4 +120,5 @@ def main():
 
 
 if __name__=="__main__":
+    logging.info("**********************mason_p nn_design(%s)***********************" % TM)
     main()